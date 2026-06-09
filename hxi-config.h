a2ca11fd7b963f23e9bb041d2180074aefa498f2
David Slik <dslik@acm.org>
2026-06-08 21:33:17 -0700

    Add config JSON validation

---
+++ hxi-config.h
@@ -0,0 +1,10 @@
+/*
+## Configuration file verification
+
+Validates the JSON-formatted configuration file
+*/
+
+    #include "cJSON.h"     // cJSON JSON parsing library
+
+    int32_t hxi_config_json_validate(cJSON* config_json);
+
