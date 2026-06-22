c6481522b3e13fd9a4b1009737ed8f0b1e914d6f
David Slik <dslik@acm.org>
2026-06-22 00:48:25 -0700

    Fixed force line wrapping, added more type conversion helpers

--- asdfg-c-types.h
+++ asdfg-c-types.h
@@ -1,7 +1,11 @@
 #include <sys/types.h>
+#include <time.h>           // POSIX time_t type, used by int64_from_time and time_from_int64
 
 int32_t int32_from_int(int input);
 int int_from_int32(int32_t input);
 
 int64_t int64_from_ssize(ssize_t input);
 ssize_t ssize_from_int64(int64_t input);
+
+int64_t int64_from_time(time_t input);
+time_t time_from_int64(int64_t input);
