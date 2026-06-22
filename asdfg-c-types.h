4daa58783c4b8098acbc17e882e782634a6753ec
David Slik <dslik@acm.org>
2026-06-22 00:12:34 -0700

    Add more type conversion routines, tests

--- asdfg-c-types.h
+++ asdfg-c-types.h
@@ -1,4 +1,7 @@
-
+#include <sys/types.h>
 
 int32_t int32_from_int(int input);
 int int_from_int32(int32_t input);
+
+int64_t int64_from_ssize(ssize_t input);
+ssize_t ssize_from_int64(int64_t input);
